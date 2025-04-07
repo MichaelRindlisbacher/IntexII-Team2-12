f1fbee9474cb14385e739047dc5c506d639fd97b
MichaelRindlisbacher <micrindi2020@gmail.com>
2025-04-07 11:27:16 -0600

    little change again

--- backend/RootkitAuth.API/Program.cs
+++ backend/RootkitAuth.API/Program.cs
@@ -64,8 +64,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors("AllowFrontend");
 app.UseHttpsRedirection();
+app.UseCors("AllowFrontend");
 
 app.UseAuthentication();
 app.UseAuthorization();
